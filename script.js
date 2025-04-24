72a329f6f084ada72354b01c574e3b82d60e02b1
unknown <ezekuiel100@gmail.com>
2025-04-24 16:29:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+
+let x = 0,
+  y = 0;
+
+let keyboard = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowLeft: false,
+  ArrowRight: false,
+};
+
+window.addEventListener("keydown", (e) => (keyboard[e.key] = true));
+window.addEventListener("keyup", (e) => (keyboard[e.key] = false));
+
+function draw() {
+  ctx.fillStyle = "orange";
+  ctx.clearRect(0, 0, 500, 400);
+  ctx.fillRect(x, y, 20, 20);
+
+  if (keyboard.ArrowUp) {
+    y -= 3;
+  } else if (keyboard.ArrowDown) {
+    y += 3;
+  } else if (keyboard.ArrowLeft) {
+    x -= 3;
+  } else if (keyboard.ArrowRight) {
+    x += 3;
+  }
+}
+
+draw();
+
+setInterval(draw, 10);
