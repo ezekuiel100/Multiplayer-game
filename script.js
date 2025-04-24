53e194082d635792ffa6e1f342e214970bf543ee
unknown <ezekuiel100@gmail.com>
2025-04-24 18:44:04 -0300

    add WebSocket client connection to server

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 
+const socket = new WebSocket("http://localhost:8080");
+
+socket.addEventListener("open", (e) => {
+  socket.send("Hello server!");
+});
+
+socket.addEventListener("message", (e) => {
+  console.log("Message from server ", e.data);
+});
+
 let x = 0,
   y = 0;
 
