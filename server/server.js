f1fb043c9f68348943c1074b6593e53a2bc1a082
unknown <ezekuiel100@gmail.com>
2025-04-25 19:35:57 -0300

    Isolate player state per socket connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
-let canvasSize = { width: 0, height: 0 };
-let points = 0;
-let fruitPosition;
-let playerPosition = { type: "playerPosition", x: 250, y: 200 };
 
 wss.on("connection", (ws) => {
+  let canvasSize = { width: 0, height: 0 };
+  let fruitPosition;
+  let points = 0;
+  let playerPosition = { type: "playerPosition", x: 250, y: 200 };
+
   ws.on("message", (data) => {
     const dt = JSON.parse(data);
 
@@ -22,28 +23,28 @@ wss.on("connection", (ws) => {
   });
 
   fruitPosition = randomFruit();
-});
 
-function randomFruit() {
-  const x = Math.ceil(Math.random() * canvasSize.width);
-  const y = Math.ceil(Math.random() * canvasSize.height);
+  function randomFruit() {
+    const x = Math.ceil(Math.random() * canvasSize.width);
+    const y = Math.ceil(Math.random() * canvasSize.height);
 
-  return JSON.stringify({ type: "fruitPosition", x, y });
-}
-
-function colision() {
-  const fruit = JSON.parse(fruitPosition);
-
-  const isColliding =
-    playerPosition.x < fruit.x + 20 &&
-    playerPosition.x + 20 > fruit.x &&
-    playerPosition.y < fruit.y + 20 &&
-    playerPosition.y + 20 > fruit.y;
-
-  if (isColliding) {
-    points++;
-    fruitPosition = randomFruit();
+    return JSON.stringify({ type: "fruitPosition", x, y });
   }
-}
+
+  function colision() {
+    const fruit = JSON.parse(fruitPosition);
+
+    const isColliding =
+      playerPosition.x < fruit.x + 20 &&
+      playerPosition.x + 20 > fruit.x &&
+      playerPosition.y < fruit.y + 20 &&
+      playerPosition.y + 20 > fruit.y;
+
+    if (isColliding) {
+      points++;
+      fruitPosition = randomFruit();
+    }
+  }
+});
 
 console.log("Servidor WebSocket rodando em ws://localhost:8080");
