f9e172bf9befb4532308d717b49e9e7a1fd8771f
unknown <ezekuiel100@gmail.com>
2025-04-24 21:32:47 -0300

    generate random fruit position

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
+let canvasSize = { width: 0, height: 0 };
 
 wss.on("connection", (ws) => {
-  ws.on("message", (data) => console.log("received: ", data.toString()));
+  ws.on("message", (data) => {
+    canvasSize = JSON.parse(data.toString());
+  });
 
-  ws.send("something");
+  ws.send(randomFruit());
 });
+
+function randomFruit() {
+  const x = Math.ceil(Math.random() * canvasSize.width);
+  const y = Math.ceil(Math.random() * canvasSize.height);
+
+  return JSON.stringify({ x, y });
+}
 
 console.log("Servidor WebSocket rodando em ws://localhost:8080");
