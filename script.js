a3f67c95072eac48834ff4f1a611a22b9a0b0254
unknown <ezekuiel100@gmail.com>
2025-05-01 18:25:20 -0300

    Restructure codebase for improved readability

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const canvas = document.querySelector("canvas");
-let ctx = canvas.getContext("2d");
-
 const player = document.querySelector("#player");
 const points = document.querySelector("#points");
 const stats = document.querySelector(".stats");
 
+let ctx = canvas.getContext("2d");
+
+const fruitSize = { width: 20, height: 20 };
+
 let fruit;
-let fruitSize = { width: 20, height: 20 };
 let playerId;
 let myPosition;
 let players;
 
+let keyboard = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowLeft: false,
+  ArrowRight: false,
+};
+
+window.addEventListener("keydown", (e) => (keyboard[e.key] = true));
+window.addEventListener("keyup", (e) => (keyboard[e.key] = false));
+
 const socket = new WebSocket("ws://localhost:8080");
 
-socket.addEventListener("open", (e) => {
+socket.addEventListener("open", () => {
+  sendCanvasSize();
+});
+
+socket.addEventListener("message", (e) => {
+  const msg = JSON.parse(e.data);
+  handleMessage(msg);
+});
+
+function sendCanvasSize() {
   const canvasSize = {
     type: "canvasSize",
     width: canvas.width - fruitSize.width,
@@ -21,11 +41,9 @@ socket.addEventListener("open", (e) => {
   };
 
   socket.send(JSON.stringify(canvasSize));
-});
+}
 
-socket.addEventListener("message", (e) => {
-  const msg = JSON.parse(e.data);
-
+function handleMessage(msg) {
   if (msg.type === "fruitPosition") {
     fruit = msg;
   }
@@ -38,29 +56,37 @@ socket.addEventListener("message", (e) => {
     myPosition = msg.players[playerId];
     players = msg.players;
   }
-});
+}
 
-let keyboard = {
-  ArrowUp: false,
-  ArrowDown: false,
-  ArrowLeft: false,
-  ArrowRight: false,
-};
+function handleMovement() {
+  if (!myPosition) return;
 
-window.addEventListener("keydown", (e) => (keyboard[e.key] = true));
-window.addEventListener("keyup", (e) => (keyboard[e.key] = false));
+  let oldX = myPosition.x;
+  let oldY = myPosition.y;
 
-function draw() {
-  ctx.clearRect(0, 0, 500, 400);
+  if (keyboard.ArrowUp) myPosition.y -= 2;
+  if (keyboard.ArrowDown) myPosition.y += 2;
+  if (keyboard.ArrowLeft) myPosition.x -= 2;
+  if (keyboard.ArrowRight) myPosition.x += 2;
 
-  if (socket.readyState === 1) {
-    drawFruit();
-    drawPlayers();
-    keyboardInput();
-    handleStats();
+  if (oldX !== myPosition.x || oldY !== myPosition.y) {
+    sendPlayerMovement();
   }
+}
 
-  requestAnimationFrame(draw);
+function sendPlayerMovement() {
+  socket.send(
+    JSON.stringify({
+      type: "move",
+      playerId,
+      x: myPosition.x,
+      y: myPosition.y,
+    })
+  );
+}
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function drawPlayers() {
@@ -77,30 +103,7 @@ function drawFruit() {
   ctx.fillRect(fruit.x, fruit.y, fruitSize.width, fruitSize.height);
 }
 
-function keyboardInput() {
-  if (!myPosition) return;
-
-  let oldX = myPosition.x;
-  let oldY = myPosition.y;
-
-  if (keyboard.ArrowUp) myPosition.y -= 2;
-  if (keyboard.ArrowDown) myPosition.y += 2;
-  if (keyboard.ArrowLeft) myPosition.x -= 2;
-  if (keyboard.ArrowRight) myPosition.x += 2;
-
-  if (oldX !== myPosition.x || oldY !== myPosition.y) {
-    socket.send(
-      JSON.stringify({
-        type: "move",
-        playerId,
-        x: myPosition.x,
-        y: myPosition.y,
-      })
-    );
-  }
-}
-
-function handleStats() {
+function updateStats() {
   if (!players) return;
 
   stats.innerHTML = "";
@@ -112,4 +115,17 @@ function handleStats() {
   }
 }
 
-requestAnimationFrame(draw);
+function gameLoop() {
+  clearCanvas();
+
+  if (socket.readyState === 1) {
+    drawFruit();
+    drawPlayers();
+    handleMovement();
+    updateStats();
+  }
+
+  requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
